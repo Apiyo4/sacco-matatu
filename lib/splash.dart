9fa65b01c764f6470436fff299ee39dce63285a0
Perpetua Mary <perpetuamary@yahoo.com>
2019-08-26 17:26:31 +0300

    class SplashState extends the State class. Override the initState method and add 5 seconds timer. Make the splash state lead to login page after the 5 seconds

--- lib/splash.dart
+++ lib/splash.dart
@@ -2,3 +2,11 @@ class Splash extends StatefulWidget {
   @override
   _SplashState createState() => _SplashState();
 }
+class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 5),() => Navigator.pushNamed(context, "/login"));
+  }
