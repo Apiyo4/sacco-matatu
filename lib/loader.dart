d21cae044adae36bf055e45358da069f4a8f1975
Perpetua Mary <perpetuamary@yahoo.com>
2019-08-26 17:59:52 +0300

    Let class LoaderState extend Loader state. Declare controller that contains variable animation_rotation that is incharge of how the loader rotates, animation_radius_in and animation_radius_out which take care of how the loader behaves. Create radius variables too.

--- lib/loader.dart
+++ lib/loader.dart
@@ -4,3 +4,13 @@ class Loader extends StatefulWidget {
   @override
   _LoaderState createState() => _LoaderState();
 }
+class _LoaderState extends State<Loader> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation_rotation;
+  Animation<double> animation_radius_in;
+  Animation<double> animation_radius_out;
+
+  final double initialradius = 25.0;
+  double radius = 0.0;
+
+}
