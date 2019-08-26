836836ce1f27ff7305fb2690d24e37a531f2fdd4
Perpetua Mary <perpetuamary@yahoo.com>
2019-08-26 17:32:37 +0300

    Create  login.dart. Let the Login clas extend the StatelessWidget class.

---
+++ lib/login.dart
@@ -0,0 +1,18 @@
+import 'package:first_app/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Color(0xFFFB415B),
+          fontFamily: "Ubuntu"
+      ),
+      home: LoginPage(),
+    );
+
+  }
+}
