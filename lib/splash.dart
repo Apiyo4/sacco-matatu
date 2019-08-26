538e0f237dba19350719fca9893060bb58e96eaf
Perpetua Mary <perpetuamary@yahoo.com>
2019-08-26 17:28:49 +0300

    Add an image to represnt the logo.Add necessary styles and color.

--- lib/splash.dart
+++ lib/splash.dart
@@ -10,3 +10,55 @@ class _SplashState extends State<Splash> {
     super.initState();
     Timer(Duration(seconds: 5),() => Navigator.pushNamed(context, "/login"));
   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: new Color(0xFFFFFFFF),
+                gradient: LinearGradient(
+                  colors: [new Color(0xFF2596A8), new Color(0xFF2D2B60)],
+                  begin: Alignment.centerRight,
+                  end: new Alignment(-0.5, -0.5),
+//              end: Alignment.centerLeft,
+                )
+
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Color(0xFFFFFFFF),
+                radius: 60.0,
+                child: Icon(
+                  Icons.airport_shuttle,
+                  color: Color(0xFF2596A8),
+                  size: 70.0,
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(top: 100.0),
+              ),
+
+              Loader()
+
+            ],
+          )
+        ],
+      ),
+    );
+
+  }
+}
+
+
+
+
+
